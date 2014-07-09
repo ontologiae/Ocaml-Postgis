42ba9e2ac73cfefbe578ad13eccb0087d3208499
Pierre-Alex <ontologiae@gmail.com>
2014-07-09 11:44:06 +0200

    Adding postgis.mli

--- postgis.ml
+++ postgis.ml
@@ -1,7 +1,7 @@
 (*#require "postgresql";;*)
 open Postgresql
 
-type pgis = [ `Postgis | `Brut ];;
+type pgis = [ `Postgis | `N ];;
 
 type ocaml_result_type =
         | Text of string
@@ -21,48 +21,39 @@ type typed_result = {
 }
 
 
-
-let get_all_with_type (result : Postgresql.result)=
-            let convert valu pgty =
-                    match pgty with
-                    | TEXT | CSTRING | VARCHAR                          -> Text valu
-                    | INT4 | INT8 | INT8 | OID | XID | CID              -> Int (int_of_string valu)
-                    | TIMETZ | TIMESTAMPTZ | TIMESTAMP | TIME           -> Date valu
-                    | NUMERIC | FLOAT8 | FLOAT4 | CASH                  -> Num (float_of_string valu)
-                    | CHAR                                              -> Char (String.get valu 0)
-                    | BOOL                                              -> Bool (bool_of_string valu)
-                    | BYTEA                                             -> Blob valu
-                    | _                                                 -> failwith "non géré"
-            in
-            let nfields = result#nfields in
-            let contruitLigne nligne = Array.init nfields (fun t -> let pgt = result#ftype t in
-                                         { value  = convert (result#getvalue nligne t ) pgt;
-                                           pgtype = pgt;
-                                         } ) in
-            let nbligne = result#ntuples in
-            Array.init nbligne (fun t -> contruitLigne t)
-
-let get_all_with_format type_array (result : Postgresql.result)  =
-        let convert exc valu pgty =
+let convertFromPgType exc valu pgty =
                 match exc with (*TODO : on regarde si on a pas un type spécial. Si oui on parse, sinon on laisse le classique*)
                 | `Postgis  -> Postgis (Wktparse.parse valu)
                 | _         ->
                  (   match pgty with
                     | TEXT | CSTRING | VARCHAR                          -> Text valu
-                    | INT4 | INT8 | INT8 | OID | XID | CID              -> Int (int_of_string valu)
+                    | INT4 | INT8  | OID | XID | CID                    -> Int (int_of_string valu)
                     | TIMETZ | TIMESTAMPTZ | TIMESTAMP | TIME           -> Date valu
                     | NUMERIC | FLOAT8 | FLOAT4 | CASH                  -> Num (float_of_string valu)
                     | CHAR                                              -> Char (String.get valu 0)
                     | BOOL                                              -> Bool (bool_of_string valu)
                     | BYTEA                                             -> Blob valu
                     | _                                                 -> failwith "non géré"
-                 )
-            in
+                 );;
+
+
+
+let get_all_with_type (result : Postgresql.result)=
+            let nfields = result#nfields in
+            let contruitLigne nligne = Array.init nfields (fun t -> let pgt = result#ftype t in
+                                         { value  = convertFromPgType `Brut (result#getvalue nligne t ) pgt;
+                                           pgtype = pgt;
+                                         } ) in
+            let nbligne = result#ntuples in
+            Array.init nbligne (fun t -> contruitLigne t)
+
+
+let get_all_with_format type_array (result : Postgresql.result)  =
             let nfields = result#nfields in
             let contruitLigne nligne = Array.init nfields (fun t -> let pgt = result#ftype t in
                                                                     let mask = type_array.(t) in
                                                                     
-                                         { value  = convert mask (result#getvalue nligne t ) pgt;
+                                         { value  = convertFromPgType mask (result#getvalue nligne t ) pgt;
                                            pgtype = pgt;
                                          } ) in
             let nbligne = result#ntuples in
---
+++ postgis.mli
@@ -0,0 +1,20 @@
+type pgis = [ `N | `Postgis ]
+type ocaml_result_type =
+        Text of string
+              | Date of string
+              | Int of int
+              | Num of float
+              | Char of char
+              | Bool of bool
+              | Postgis of Syntax.wkt
+              | NumRange of float * float
+              | Blob of string
+              | Null
+type typed_result = { value : ocaml_result_type; pgtype : Postgresql.ftype; }
+val convertFromPgType :
+        [> `Postgis ] -> string -> Postgresql.ftype -> ocaml_result_type
+val get_all_with_type : Postgresql.result -> typed_result array array
+val get_all_with_format :
+        [> `Postgis ] array -> Postgresql.result -> typed_result
+        array array
+
